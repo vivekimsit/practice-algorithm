c55118d4bfbb4e0e74f38acd9732c1c4259d5f2c
Vivek Poddar <vivekimsit@gmail.com>
2018-05-16 17:05:35 +0100

    WIP.

---
+++ IC/ParenthesisValidator.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+// for any state change check pre and post conditions.
+class ParenthesisValidator {
+  static boolean solve(String text) {
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < text.length(); i++) {
+      char ch = text.charAt(0);
+      if ("{[(".indexOf(ch) > 0) {
+        stack.push(ch);
+      }
+      else if ("}])".indexOf(ch) > 0) {
+        char top = stack.pop();
+      }
+    }
+
+    return stack.empty();
+  }
+
+  public static void main(String[] args) {
+  
+  }
+}
