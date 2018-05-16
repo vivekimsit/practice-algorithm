d48650d0c9cd858155ad7174479119d68fda8252
Vivek Poddar <vivekimsit@gmail.com>
2018-05-16 19:50:48 +0100

    Parenthesis validator.

--- IC/ParenthesisValidator.java
+++ IC/ParenthesisValidator.java
@@ -1,16 +1,21 @@
 import java.util.Stack;
 
-// for any state change check pre and post conditions.
+
 class ParenthesisValidator {
-  static boolean solve(String text) {
-    Stack<Character> stack = new Stack<>();
+  static boolean solve(final String text) {
+    if (text == null) return true;
+
+    final Stack<Character> stack = new Stack<>();
 
     for (int i = 0; i < text.length(); i++) {
-      char ch = text.charAt(0);
+      char ch = text.charAt(i);
       if ("{[(".indexOf(ch) > 0) {
         stack.push(ch);
       }
       else if ("}])".indexOf(ch) > 0) {
+        if (stack.size() == 0) {
+          return false;
+        }
         char top = stack.peek();
         if (top == '{' && ch == '}') {
           stack.pop();
@@ -28,10 +33,10 @@ class ParenthesisValidator {
   }
 
   public static void main(String[] args) {
-    solve(null);
-    solve("");
-    solve("[{(");
-    solve("[]");
-    solve("[{()}]");
+    System.out.println(solve(null));
+    System.out.println(solve(""));
+    System.out.println(solve("[{("));
+    System.out.println(solve("[]"));
+    System.out.println(solve("[{()}]"));
   }
 }
