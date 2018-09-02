42b3902fb1830f7b3944541457361672c7275ac8
Vivek Poddar <vivekimsit@gmail.com>
2018-09-02 21:16:13 +0100

    Pre order recursive.

--- LC/tree/PreOrderTraversal.java
+++ LC/tree/PreOrderTraversal.java
@@ -1,25 +1,19 @@
 import java.util.*;
 
 public class PreOrderTraversal {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            result.add(node.val);
-            if (node.right != null) {
-                stack.push(node.right);
-            }
-            if (node.left != null) {
-                stack.push(node.left);
-            }
-        }
-        return result;
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    dfs(root, result);
+    return result;
+  }
+
+  private void dfs(TreeNode root, List<Integer> rsf) {
+    if (root != null) {
+      rsf.add(root.val);
+      dfs(root.left,  rsf);
+      dfs(root.right, rsf);
     }
+  }
 }
 
 class PreOrderTraversalIterative {
