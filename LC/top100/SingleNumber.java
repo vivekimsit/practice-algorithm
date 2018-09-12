0e63c9ea7ba43527e609f5d582861f0c8dd73d47
Vivek Poddar <vivekimsit@gmail.com>
2018-09-12 20:06:43 +0100

    Single number.

---
+++ LC/top100/SingleNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class SingleNumber {
+  public int singleNumber(int[] nums) {
+    int result = 0;
+    for (int num : nums) {
+      result ^= num;
+    }
+    return result;
+  }
+}
